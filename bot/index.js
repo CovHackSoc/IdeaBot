0d15dfd96e2b1192a77d3ade90aeeea3e675fdb5
bahorn <hornb@uni.coventry.ac.uk>
2019-02-07 23:19:19 +0000

    Looks for keyword now.

--- bot/index.js
+++ bot/index.js
@@ -1,17 +1,24 @@
+/* eslint no-console: 0 */
 const Discord = require('discord.js');
-
-const config = require('dotenv').config();
+const shlex = require('shlex');
+const dotenv = require('dotenv');
 
 const client = new Discord.Client();
+
+dotenv.config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', (msg) => {
+  const { content, author } = msg;
+  const message = shlex.split(content);
+  if (message[0] === '!idea') {
+    /* Process here */
+    console.log(message);
   }
 });
+
 
 client.login(process.env.TOKEN);
